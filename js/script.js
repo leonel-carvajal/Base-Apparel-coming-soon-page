bea392a19c24b71d4927e0223c5ad0b8abf8c098
leonel-carvajal <liosoft777@gmail.com>
2021-03-06 18:37:48 -0600

    final v0.1

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const email = document.getElementById('email')
+const form = document.getElementById('form')
+const button = document.getElementById('button')
+const footerError = document.getElementById('footer__error')
+const modal = document.getElementById('modal')
+const IconClose = document.getElementById('icon__close')
+
+form.addEventListener('submit', (e) => e.preventDefault())
+IconClose.addEventListener('click', () => modal.classList.remove('show'))
+
+const validateEmail = (data) => {
+  const emailRegex = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  if (emailRegex.test(data)) {
+    modal.classList.add('show')
+    email.value = ''
+    email.classList.remove('error')
+    email.classList.remove('footer__email--active')
+    button.classList.remove('footer__button--send')
+  } 
+  else {
+    email.classList.add('error')
+    email.classList.add('footer__email--active')
+    button.classList.add('footer__button--send')
+
+  } 
+}
+
+button.addEventListener('click', () => {
+  validateEmail(email.value)
+})
